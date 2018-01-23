acfc8fe2ac923b6a472b9f10dc8cbba0f5568241
moannuo <demenet.alain@hotmail.fr>
2018-01-23 17:38:22 +0100

    ft_memset done

---
+++ ft_memset.s
@@ -0,0 +1,16 @@
+section .text
+	global _ft_memset
+
+_ft_memset:
+	enter		0, 0
+	cmp			rbx, 0
+	je			end
+	mov			rbx, rdi	; save 1st arg
+	mov			rax, rsi
+	mov			rcx, rdx
+	cld
+    rep			stosb
+end:
+	mov			rax, rbx
+	leave
+	ret
--- libfts.h
+++ libfts.h
@@ -6,12 +6,14 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 10:54:51 by ademenet          #+#    #+#             */
-/*   Updated: 2018/01/23 11:46:44 by ademenet         ###   ########.fr       */
+/*   Updated: 2018/01/23 14:55:19 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTS_H
 # define LIBFTS_H
+
+# include <unistd.h>
 
 /*
 ** MANDATORY PART
@@ -23,6 +25,7 @@ int             ft_isalpha(int c);
 int             ft_isascii(int c);
 int             ft_isdigit(int c);
 int             ft_isprint(int c);
+void	        *ft_memset(void *b, int c, size_t len);
 void	        ft_puts(char const *s);
 char	        *ft_strcat(char *s1, const char *s2);
 size_t			ft_strlen(const char *s);
